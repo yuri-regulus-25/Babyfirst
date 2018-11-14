00febffbfab9661b8f4bf147e54a23c59239b3ff
guxtuti-raven <s1712033@s.do-johodai.ac.jp>
2018-11-15 05:13:38 +0900

    close #2 カラーコードの修正

--- app/src/main/java/jp/ac/yukine/babyfirst/MainActivity.java
+++ app/src/main/java/jp/ac/yukine/babyfirst/MainActivity.java
@@ -12,6 +12,8 @@ import android.view.View;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    public boolean inspection = false;
+    public int color_apply = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,80 +38,76 @@ public class MainActivity extends AppCompatActivity {
     {
         ConstraintLayout Background_Color = (ConstraintLayout) findViewById(R.id.backgroundcheck);
         Random rand = new Random();
-        int color_apply = rand.nextInt(10);
+
+        if(inspection)
+            if(color_apply == 10)
+                color_apply = 0;
+            else
+                color_apply++;
+
+        else
+            color_apply = rand.nextInt(10);
 
         switch (color_apply)
         {
             case 0:
                 //赤色
                 Background_Color.setBackgroundColor(Color.rgb(210, 00, 45));
-                
                 break;
 
             case 1:
                 //オレンジ
                 Background_Color.setBackgroundColor(Color.rgb(255, 102, 00));
-                
                 break;
 
             case 2:
                 //黄色
                 Background_Color.setBackgroundColor(Color.rgb(255, 204, 00));
-                
                 break;
 
             case 3:
                 //緑色
                 Background_Color.setBackgroundColor(Color.rgb(50, 197, 00));
-                
                 break;
 
             case 4:
                 //水色
                 Background_Color.setBackgroundColor(Color.rgb(72, 199, 255));
-                
                 break;
 
             case 5:
                 //青色
-                Background_Color.setBackgroundColor(Color.rgb(00, 85, 255));
-                
+                Background_Color.setBackgroundColor(Color.rgb(00, 109, 230));
                 break;
 
             case 6:
                 //紫色
-                Background_Color.setBackgroundColor(Color.rgb(127, 40, 255));
-                
+                Background_Color.setBackgroundColor(Color.rgb(135, 55, 225));
                 break;
 
             case 7:
                 //ピンク
                 Background_Color.setBackgroundColor(Color.rgb(255,152, 183));
-                
                 break;
 
             case 8:
                 //茶色
                 Background_Color.setBackgroundColor(Color.rgb(100, 64, 60));
-                
                 break;
 
             case 9:
                 //黒
                 Background_Color.setBackgroundColor(Color.rgb(0, 0, 0));
-                
                 break;
 
             case 10:
                 //灰色
                 Background_Color.setBackgroundColor(Color.rgb(147, 147, 147));
-                
                 break;
 
             default:
                 //例外処理
                 Background_Color.setBackgroundColor(Color.rgb(255, 255, 255));
-                
                 break;
         }
         System.out.println("Random: 色: " + color_apply);
